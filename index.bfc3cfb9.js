352cb6c13329ac1643232f686ea211f165b88cc7
Anzhelika-Light <anzhelikaantoniuk1202@gmail.com>
2023-01-19 13:37:58 +0000

    Deploying to gh-pages from @ Anzhelika-Light/goit-js-hw-10@0f0b8007ca7588ca6c9f3c7013ad08acb87fa573 🚀

--- index.bfc3cfb9.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("#search-box");document.querySelector(".country-list");t.addEventListener("click",(function(){fetch("https://jsonplaceholder.typicode.com/users").then((function(t){if(!t.ok)throw new Error(t.status);return t.json()})).then((function(t){return renderCountryList(t)})).catch((function(t){return console.log(t)}))}))}();
-//# sourceMappingURL=index.bfc3cfb9.js.map
