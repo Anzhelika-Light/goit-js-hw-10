9e039027c5ce0d0b880cfc795ff6d97bf669b35c
Anzhelika-Light <anzhelikaantoniuk1202@gmail.com>
2023-01-19 11:43:09 +0200

    changed branch for deploy

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const InputEl = document.querySelector("#search-box");
+const countryList = document.querySelector(".country-list");
+
+InputEl.addEventListener("click", () => {
+  fetchCountries()
+    .then((countries) => renderCountryList(countries))
+    .catch((error) => console.log(error));
+});
+
+function fetchCountries() {
+  return fetch("https://jsonplaceholder.typicode.com/users").then(
+    (response) => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    }
+  );
+}
